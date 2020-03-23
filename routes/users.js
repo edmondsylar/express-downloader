05d274399a83f474cd2199f872ce49a027d7816a
edmondsylar <ukwelys@gmail.com>
2020-03-23 04:46:49 -0700

    added database connection

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('./db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var name = req.param('username')
-  res.render('users', { title: 'Express Route for users', username: name});
-  // res.send('respond with a resource');
+  connection.query("select * from imageurl where status='New'", function(err, rows, fields){
+    if(err) throw err
+
+    var data = rows;
+    console.log(data);
+    res.render('users', { title: 'New url\'s', username: name, newRecords: data });
+  });
+
+  // connection.end();
+  return false;
 });
 
 module.exports = router;
